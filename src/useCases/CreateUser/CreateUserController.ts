399e53cb4186f403a0d211eed7a919ff6b9e7456
jpgcarvalho <peugomescarvalho2@gmail.com>
2022-05-25 14:29:31 -0300

    first commit

---
+++ src/useCases/CreateUser/CreateUserController.ts
@@ -0,0 +1,28 @@
+import { Response, Request } from "express";
+import { CreateUserUseCase } from "./CreateUserUseCase";
+
+export class CreateUserController{
+    private createUserUseCase: CreateUserUseCase;
+    constructor(
+        createUserUseCase: CreateUserUseCase
+    ){
+        this.createUserUseCase = createUserUseCase;
+    }
+
+    async handle(request: Request, response: Response): Promise<Response> {
+        const { name, email, password } = request.body;
+
+        try {
+            await this.createUserUseCase.execute({
+                name, 
+                email,
+                password
+            })
+            return response.status(201).send();
+        } catch (error) {
+            return response.status(400).json({
+                message: error.message || 'Unexpected error'
+            })
+        }
+    }
+}
